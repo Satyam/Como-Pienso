a9379ddfdde235facd6b20bc377b139b79eb8ec4
Satyam <satyam@satyam.com.ar>
2020-08-11 19:37:18 +0200

    Improved on the front page

--- v2-website/src/pages/index.js
+++ v2-website/src/pages/index.js
@@ -1,18 +1,19 @@
 import Layout from '@theme/Layout';
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+
 export default () => {
   return (
     <Layout>
       <div className="alignCenter">
-        <h2>Cómo Pienso</h2>
+        <h1>Cómo Pienso</h1>
         <a href="https://commons.wikimedia.org/w/index.php?curid=123075">
           <img
             src={useBaseUrl('img/Portada.jpg')}
             alt="De Adrian Pingstone - Fotografía propia, Dominio público'"
           />
         </a>
-        <p>Rumiando algunas ideas</p>
+        <h3>Rumiando algunas ideas</h3>
         <div>
           <a href={useBaseUrl('docs')}>Introducción</a>
         </div>
